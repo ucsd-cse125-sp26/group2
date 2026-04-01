4edd28a6e8fe34cacadd6dd4892ff47abccead6c
Daniel Katulevskiy <katulevskiy@gmail.com>
2026-03-31 20:44:57 -0700

    windows release fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,11 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-#include <entt/entt.hpp>
-#include <glm/glm.hpp>
-
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
+#include <entt/entt.hpp>
+#include <glm/glm.hpp>
 #include <string>
 
 // ---------------------------------------------------------------------------
@@ -16,7 +15,8 @@
 
 // Returns the path to a compiled shader (.spv) file relative to the binary.
 // Looks inside a "shaders/" subdirectory next to the executable.
-static std::string shaderPath(const char* filename) {
+static std::string shaderPath(const char* filename)
+{
     const char* base = SDL_GetBasePath();
     std::string path = base ? base : "./";
     path += "shaders/";
@@ -24,15 +24,14 @@ static std::string shaderPath(const char* filename) {
     return path;
 }
 
-static SDL_GPUShader* loadShader(
-    SDL_GPUDevice* gpu,
-    const char* filename,
-    SDL_GPUShaderStage stage,
-    Uint32 samplerCount,
-    Uint32 uniformBufferCount,
-    Uint32 storageBufferCount,
-    Uint32 storageTextureCount
-) {
+static SDL_GPUShader* loadShader(SDL_GPUDevice* gpu,
+                                 const char* filename,
+                                 SDL_GPUShaderStage stage,
+                                 Uint32 samplerCount,
+                                 Uint32 uniformBufferCount,
+                                 Uint32 storageBufferCount,
+                                 Uint32 storageTextureCount)
+{
     std::string path = shaderPath(filename);
     size_t codeSize  = 0;
     void* code       = SDL_LoadFile(path.c_str(), &codeSize);
@@ -42,15 +41,15 @@ static SDL_GPUShader* loadShader(
     }
 
     SDL_GPUShaderCreateInfo info{};
-    info.code                  = static_cast<const Uint8*>(code);
-    info.code_size             = codeSize;
-    info.entrypoint            = "main";
-    info.format                = SDL_GPU_SHADERFORMAT_SPIRV;
-    info.stage                 = stage;
-    info.num_samplers          = samplerCount;
-    info.num_uniform_buffers   = uniformBufferCount;
-    info.num_storage_buffers   = storageBufferCount;
-    info.num_storage_textures  = storageTextureCount;
+    info.code                 = static_cast<const Uint8*>(code);
+    info.code_size            = codeSize;
+    info.entrypoint           = "main";
+    info.format               = SDL_GPU_SHADERFORMAT_SPIRV;
+    info.stage                = stage;
+    info.num_samplers         = samplerCount;
+    info.num_uniform_buffers  = uniformBufferCount;
+    info.num_storage_buffers  = storageBufferCount;
+    info.num_storage_textures = storageTextureCount;
 
     SDL_GPUShader* shader = SDL_CreateGPUShader(gpu, &info);
     SDL_free(code);
@@ -61,18 +60,20 @@ static SDL_GPUShader* loadShader(
 // App state
 // ---------------------------------------------------------------------------
 
-struct AppState {
-    SDL_Window*           window   = nullptr;
-    SDL_GPUDevice*        gpu      = nullptr;
+struct AppState
+{
+    SDL_Window* window                = nullptr;
+    SDL_GPUDevice* gpu                = nullptr;
     SDL_GPUGraphicsPipeline* pipeline = nullptr;
-    entt::registry        registry;
+    entt::registry registry;
 };
 
 // ---------------------------------------------------------------------------
 // Lifecycle callbacks
 // ---------------------------------------------------------------------------
 
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
     SDL_SetAppMetadata("Titandoom", "0.1.0", "com.titandoom");
 
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
@@ -83,21 +84,16 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     auto* state = new AppState();
     *appstate   = state;
 
-    state->gpu = SDL_CreateGPUDevice(
-        SDL_GPU_SHADERFORMAT_SPIRV,
-        /*debug_mode=*/true,
-        /*name=*/nullptr
-    );
+    state->gpu = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV,
+                                     /*debug_mode=*/true,
+                                     /*name=*/nullptr);
     if (!state->gpu) {
         SDL_Log("SDL_CreateGPUDevice failed: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     SDL_Log("GPU driver: %s", SDL_GetGPUDeviceDriver(state->gpu));
 
-    state->window = SDL_CreateWindow(
-        "Titandoom", 1280, 720,
-        SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY
-    );
+    state->window = SDL_CreateWindow("Titandoom", 1280, 720, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
     if (!state->window) {
         SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -109,18 +105,8 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     }
 
     // ---- Load shaders ---------------------------------------------------
-    SDL_GPUShader* vert = loadShader(
-        state->gpu,
-        "triangle.vert.spv",
-        SDL_GPU_SHADERSTAGE_VERTEX,
-        0, 0, 0, 0
-    );
-    SDL_GPUShader* frag = loadShader(
-        state->gpu,
-        "triangle.frag.spv",
-        SDL_GPU_SHADERSTAGE_FRAGMENT,
-        0, 0, 0, 0
-    );
+    SDL_GPUShader* vert = loadShader(state->gpu, "triangle.vert.spv", SDL_GPU_SHADERSTAGE_VERTEX, 0, 0, 0, 0);
+    SDL_GPUShader* frag = loadShader(state->gpu, "triangle.frag.spv", SDL_GPU_SHADERSTAGE_FRAGMENT, 0, 0, 0, 0);
     if (!vert || !frag) {
         return SDL_APP_FAILURE;
     }
@@ -130,14 +116,14 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     colorTarget.format = SDL_GetGPUSwapchainTextureFormat(state->gpu, state->window);
 
     SDL_GPUGraphicsPipelineCreateInfo pipelineInfo{};
-    pipelineInfo.vertex_shader              = vert;
-    pipelineInfo.fragment_shader            = frag;
-    pipelineInfo.primitive_type             = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST;
-    pipelineInfo.target_info.color_target_descriptions   = &colorTarget;
-    pipelineInfo.target_info.num_color_targets           = 1;
-    pipelineInfo.rasterizer_state.fill_mode              = SDL_GPU_FILLMODE_FILL;
-    pipelineInfo.rasterizer_state.cull_mode              = SDL_GPU_CULLMODE_NONE;
-    pipelineInfo.rasterizer_state.front_face             = SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE;
+    pipelineInfo.vertex_shader                         = vert;
+    pipelineInfo.fragment_shader                       = frag;
+    pipelineInfo.primitive_type                        = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST;
+    pipelineInfo.target_info.color_target_descriptions = &colorTarget;
+    pipelineInfo.target_info.num_color_targets         = 1;
+    pipelineInfo.rasterizer_state.fill_mode            = SDL_GPU_FILLMODE_FILL;
+    pipelineInfo.rasterizer_state.cull_mode            = SDL_GPU_CULLMODE_NONE;
+    pipelineInfo.rasterizer_state.front_face           = SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE;
 
     state->pipeline = SDL_CreateGPUGraphicsPipeline(state->gpu, &pipelineInfo);
 
@@ -153,7 +139,8 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
     }
@@ -163,7 +150,8 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void* appstate) {
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
     auto* state = static_cast<AppState*>(appstate);
 
     SDL_GPUCommandBuffer* cmdbuf = SDL_AcquireGPUCommandBuffer(state->gpu);
@@ -195,7 +183,8 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
     auto* state = static_cast<AppState*>(appstate);
     if (state) {
         if (state->pipeline) {
