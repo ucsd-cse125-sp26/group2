8769272d52f9c0757a8f18f9c3d664a082180de6
Wifu <j6castaneda@ucsd.edu>
2026-04-06 14:02:14 -0700

    no more "extern C" hack

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,13 +1,12 @@
+#include "main.h"
+
 #include <SDL3/SDL.h>
 
-#include <main.h>
-
-extern "C" {
 // ---------------------------------------------------------------------------
 // SDL3 app callbacks
 // ---------------------------------------------------------------------------
 
-SDL_AppResult SDL_AppInit(void** appstate, int /*argc*/, char* /*argv*/[])
+SDL_AppResult rendererInit(void** appstate, int /*argc*/, char* /*argv*/[])
 {
     SDL_SetAppMetadata("group2", "0.1.0", "com.cse125.group2");
 
@@ -40,7 +39,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int /*argc*/, char* /*argv*/[])
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void* /*appstate*/, SDL_Event* event)
+SDL_AppResult rendererAppEvent(void* /*appstate*/, SDL_Event* event)
 {
     if (event->type == SDL_EVENT_QUIT)
         return SDL_APP_SUCCESS;
@@ -49,7 +48,7 @@ SDL_AppResult SDL_AppEvent(void* /*appstate*/, SDL_Event* event)
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void* appstate)
+SDL_AppResult rendererAppIterate(void* appstate)
 {
     auto* s = static_cast<AppState*>(appstate);
 
@@ -69,14 +68,12 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void* appstate, SDL_AppResult /*result*/)
+void rendererAppQuit(void* appstate, SDL_AppResult /*result*/)
 {
     auto* s = static_cast<AppState*>(appstate);
     if (!s)
         return;
     SDL_DestroyRenderer(s->renderer);
     SDL_DestroyWindow(s->window);
-    SDL_Quit();
     delete s;
-}
 }
