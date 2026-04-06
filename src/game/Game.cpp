a19f9cfc7b683e3379cc115b47be4cae84222844
Wifu <j6castaneda@ucsd.edu>
2026-04-06 14:41:31 -0700

    Reworked core project org.

---
+++ src/game/Game.cpp
@@ -0,0 +1,50 @@
+#include "Game.hpp"
+
+bool Game::init()
+{
+    SDL_SetAppMetadata("group2", "0.1.0", "com.cse125.group2");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return false;
+    }
+
+    window = SDL_CreateWindow("group2", 1280, 720, SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+        return false;
+    }
+
+    SDL_Renderer* sdlRenderer = SDL_CreateRenderer(window, nullptr);
+    if (!sdlRenderer) {
+        SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        return false;
+    }
+    renderer = new Renderer();
+    renderer->init(sdlRenderer);
+
+    return true;
+}
+
+SDL_AppResult Game::event(SDL_Event* event)
+{
+    if (event->type == SDL_EVENT_QUIT)
+        return SDL_APP_SUCCESS;
+    if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_ESCAPE)
+        return SDL_APP_SUCCESS;
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult Game::iterate()
+{
+    renderer->drawFrame();
+    return SDL_APP_CONTINUE;
+}
+
+void Game::quit()
+{
+    renderer->quit();
+    SDL_DestroyWindow(window);
+}
