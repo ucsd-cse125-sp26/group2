41497ec3f9286d28ee956dec7ad1c9af27d0e43f
Cedric-James David <cedricjdvd@gmail.com>
2026-04-07 16:04:03 -0700

    Add Client to Game

--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -1,13 +1,14 @@
+#include "SDL3/SDL_stdinc.h"
 #include "SDL3_net/SDL_net.h"
 class Server
 {
 public:
-    bool init(const char* addr, Uint16 port, int tickRateMs);
+    bool init(const char* addr, Uint16 port, Uint32 tickRateMs);
     void shutdown();
     void run();
 
 private:
     NET_DatagramSocket* sock;
     bool running;
-    int serverTickRateMs;
+    Uint32 serverTickRateMs;
 };
