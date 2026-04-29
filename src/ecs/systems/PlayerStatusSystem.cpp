adf50c631b03d8719d5f0a344ee1191aac27edee
Cedric-James David <cedricjdvd@gmail.com>
2026-04-29 11:53:50 -0700

    fix: Remove hitbox on death

--- src/ecs/systems/PlayerStatusSystem.cpp
+++ src/ecs/systems/PlayerStatusSystem.cpp
@@ -107,6 +107,7 @@ inline void handleDeath(entt::entity& player,
         registry.get_or_emplace<PlayerState>(player).IsDead = true;
         registry.get_or_emplace<Velocity>(player) = Velocity{};
         registry.patch<Renderable>(player, [](Renderable& rend) { rend.visible = false; });
+        registry.remove<HitboxInstance>(player);
         registry.emplace_or_replace<RespawnTimer>(player, RespawnTimer{.timeRemaining = 5.0f});
         registry.patch<PlayerMatchStats>(player, [&](PlayerMatchStats& stats) { stats.deaths++; });
 
