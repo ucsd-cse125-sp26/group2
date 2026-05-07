e28e06f1a2e27bade2f73728a849620ce390ebb6
Jacob Roner <jroner@ucsd.edu>
2026-05-07 16:06:15 -0700

    Added ability state to ECS

---
+++ src/ecs/components/AbilityConfig.hpp
@@ -0,0 +1,7 @@
+/// @file AbilityConfig.hpp
+/// @brief Contains config information for abilities and level system.
+
+#pragma once
+
+constexpr float dmgThreshold = 10000.0f;
+constexpr int maxLevel = 2;
